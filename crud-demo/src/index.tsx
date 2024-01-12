e7a5f33ea78f5661a89054c9c20322805b7286aa
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-12 16:25:56 -0600

    progress

--- crud-demo/src/index.tsx
+++ crud-demo/src/index.tsx
@@ -1,9 +1,14 @@
 import {Hono, type Context} from 'hono';
 import {serveStatic} from 'hono/bun';
+import {logger} from 'hono/logger';
 import dogRouter from './dog-router';
 
 const app = new Hono();
 
+// This logs all HTTP requests to the terminal where the server is running.
+app.use('/*', logger());
+
+// This serves static files from the public directory.
 // Try browsing http://localhost:3000/demo.html
 app.use('/*', serveStatic({root: './public'}));
 
