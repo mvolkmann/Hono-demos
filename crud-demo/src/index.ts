46737390a42031419f45849c69bc6e20cbbd1eb3
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-12 14:49:26 -0600

    progress

--- crud-demo/src/index.ts
+++ crud-demo/src/index.ts
@@ -1,19 +1,31 @@
 import {Hono, type Context} from 'hono';
 import {serveStatic} from 'hono/bun';
 
-type Dog = {
-  id: number;
+interface NewDog {
   name: string;
   breed: string;
-};
+}
 
-const dogs: {[id: number]: Dog} = {
-  1: {id: 1, name: 'Comet', breed: 'Whippet'},
-  2: {id: 2, name: 'Oscar', breed: 'German Shorthaired Pointer'}
-};
+interface Dog extends NewDog {
+  id: number;
+}
+
+let lastId = 0;
+const dogs: {[id: number]: Dog} = {};
+
+function addDog(name: string, breed: string): Dog {
+  const id = ++lastId;
+  const dog = {id, name, breed};
+  dogs[id] = dog;
+  return dog;
+}
+
+addDog('Comet', 'Whippet');
+addDog('Oscar', 'German Shorthaired Pointer');
 
 const app = new Hono();
 
+// Try browsing http://localhost:3000/demo.html
 app.use('/*', serveStatic({root: './public'}));
 
 app.get('/dog', (c: Context) => {
@@ -27,12 +39,30 @@ app.get('/dog/:id', (c: Context) => {
   return c.json(dog);
 });
 
-/*
-app.post('/dog', (c: Context) => {
-  // const dog = c.body;
-  // console.log('index.ts post: dog =', dog);
-  c.text('demo');
+app.post('/dog', async (c: Context) => {
+  const data = (await c.req.json()) as unknown as NewDog;
+  const dog = addDog(data.name, data.breed);
+  return c.json(dog);
 });
-*/
+
+app.put('/dog/:id', async (c: Context) => {
+  const id = Number(c.req.param('id'));
+  const data = (await c.req.json()) as unknown as NewDog;
+  const dog = dogs[id];
+  if (dog) {
+    dog.name = data.name;
+    dog.breed = data.breed;
+  }
+  c.status(dog ? 200 : 404);
+  return c.json(dog);
+});
+
+app.delete('/dog/:id', async (c: Context) => {
+  const id = Number(c.req.param('id'));
+  const dog = dogs[id];
+  if (dog) delete dogs[id];
+  c.status(dog ? 200 : 404);
+  return c.text('');
+});
 
 export default app;
