5c4334adf0d3309357a71dce0a5eba54a099f959
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-10 09:15:34 -0600

    progress

---
+++ honox-file-based-routing/app/routes/_renderer.tsx
@@ -0,0 +1,13 @@
+import {jsxRenderer} from 'hono/jsx-renderer';
+
+export default jsxRenderer(({children}) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+});
